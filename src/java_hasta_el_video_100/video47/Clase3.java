77741b070c9c2f1eb5354f8694ec18ccb730782c
german <germanjongewaard@gmail.com>
2017-12-24 12:10:01 +0100

    new commit

---
+++ src/java_hasta_el_video_100/video47/Clase3.java
@@ -0,0 +1,15 @@
+package java_hasta_el_video_100.video47;
+
+import java_hasta_el_video_100.video47_1.Clase1;
+
+ /**
+ *
+ * @author german
+ * 
+ */
+
+public class Clase3 extends Clase1{
+    
+    
+    
+}
