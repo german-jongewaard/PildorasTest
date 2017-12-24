fe4cff63d8c6dd6100598bd0d2c0f620436b288e
german <germanjongewaard@gmail.com>
2017-12-24 12:10:43 +0100

    new commit

---
+++ src/java_hasta_el_video_100/video47_1/Clase1.java
@@ -0,0 +1,20 @@
+ package java_hasta_el_video_100.video47_1;
+
+ /**
+ *
+ * @author german
+ * 
+ */
+ 
+public class Clase1 {   
+    
+    protected int mivar = 5;
+    
+    protected int mivar2 = 7;
+    
+    protected String mimetodo(){//METODO GETTER
+    
+        return "El valor de mivar2 es: " + mivar2;
+}
+    
+}
