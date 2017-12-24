e66d19430eb633a618d7bab7f1c1fe7d2ffce844
german <germanjongewaard@gmail.com>
2017-12-24 12:11:42 +0100

    new commit

---
+++ src/java_hasta_el_video_100/video47_1/Clase2.java
@@ -0,0 +1,23 @@
+package java_hasta_el_video_100.video47_1;
+
+import java_hasta_el_video_100.video47.Clase3;
+
+ /**
+ *
+ * @author german
+ * 
+ */
+
+public class Clase2 {
+     
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Instancia de la Clase1
+        Clase1 miobjeto = new Clase1();
+        
+        //Instancia de la Clase3
+        Clase3 miobjeto2 = new Clase3();
+        
+    }    
+}
