18aa06c009dc39c00cfb279161bd3aa9e9f3e253
german <germanjongewaard@gmail.com>
2017-12-25 03:18:33 +0100

    new commit

---
+++ src/java_hasta_el_video_100/video51/Trabajadores.java
@@ -0,0 +1,11 @@
+ 
+package java_hasta_el_video_100.video51;
+
+ 
+public interface Trabajadores {
+    
+    double establece_bonus(double gratificacion);
+    
+    double bonus_base = 1500;
+    
+}
