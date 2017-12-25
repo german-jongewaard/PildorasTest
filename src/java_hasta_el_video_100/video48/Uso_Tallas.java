083c81f83aab776ff2498ad6ffa56e8f2cd7addd
german <germanjongewaard@gmail.com>
2017-12-25 03:10:18 +0100

    new commit

---
+++ src/java_hasta_el_video_100/video48/Uso_Tallas.java
@@ -0,0 +1,24 @@
+ 
+package java_hasta_el_video_100.video48;
+
+ 
+public class Uso_Tallas {
+
+    /*Los tipos enumerados se declaran fuera del main*/
+    
+    enum Talla{MINI, MEDIANO, GRANDE, MUY_GRANDE};
+    /*La variable objeto Talla solo puede almacenar en su 
+    interior los datos que hemos ingresado*/
+     
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Talla S = Talla.MINI;
+        Talla M = Talla.MEDIANO;
+        Talla L = Talla.GRANDE;
+        Talla XL = Talla.MUY_GRANDE;
+        
+        
+    }
+    
+}
