56b17fdb11fe0268a4ee7673521451795b91cba0
german <germanjongewaard@gmail.com>
2017-12-25 03:20:02 +0100

    new commit

---
+++ src/java_hasta_el_video_100/video51/Jefes.java
@@ -0,0 +1,15 @@
+
+package java_hasta_el_video_100.video51;
+
+/**
+ *
+ * @author german
+ * 
+ */ 
+
+ 
+public interface Jefes extends Trabajadores {
+    
+    String tomar_decisiones(String decision);
+    
+}
