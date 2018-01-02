d1e643234ac961b7744cd290757b65fc5dc1332b
german <germanjongewaard@gmail.com>
2018-01-02 08:52:35 +0100

    Instanciando la Clase1 y la Clase3

--- src/java_hasta_el_video_100/video47_1/Clase2.java
+++ src/java_hasta_el_video_100/video47_1/Clase2.java
@@ -4,15 +4,16 @@ import java_hasta_el_video_100.video47.Clase3;
 
  /**
  *
+ * 
  * @author german
+ * 
  * 
  */
 
 public class Clase2 {
      
     public static void main(String[] args) {
-        // TODO code application logic here
-        
+               
         //Instancia de la Clase1
         Clase1 miobjeto = new Clase1();
         
