e974c999c12abbebf8aa40ace68197f279a24c90
german <germanjongewaard@gmail.com>
2018-01-03 08:50:52 +0100

    Uso Campos privados - ActionListener - Timer - Temporizador

---
+++ src/java_hasta_el_video_100/video54/PruebaTemporizadorDos.java
@@ -0,0 +1,63 @@
+package java_hasta_el_video_100.video54;
+
+ 
+ 
+import java.awt.Toolkit;
+
+import javax.swing.*;
+
+import java.awt.event.*;
+
+import java.util.*;
+
+import javax.swing.Timer;
+
+ 
+
+public class PruebaTemporizadorDos {
+
+    public static void main(String[] args) {
+        
+        Reloj miReloj = new Reloj( );
+        
+        miReloj.enMarcha(3000, true);
+        
+        JOptionPane.showMessageDialog(null, "Pulsa acpetar para detener");
+        
+        System.exit(0);
+        
+
+    }
+}
+
+class Reloj{
+    
+     
+    
+    public void enMarcha(int intervalo, boolean sonido){
+        
+        class DameLaHora2 implements ActionListener{
+        
+        public void actionPerformed(ActionEvent evento){
+            
+            Date ahora = new Date();
+            System.out.println("Te pongo la hora cada 3 segundos " + ahora);
+            
+            if(sonido==true){
+                
+                Toolkit.getDefaultToolkit().beep();
+            }
+        }
+    }
+         
+        ActionListener oyente = new DameLaHora2();
+        
+        Timer mitemporizador = new Timer(intervalo, oyente);
+        
+        mitemporizador.start();
+    
+    }
+     
+    
+}
+
