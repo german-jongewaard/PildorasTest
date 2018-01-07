ac2b9f3c99a2440207a22d16fa683c854730e013
german <germanjongewaard@gmail.com>
2018-01-07 22:13:41 +0100

    Foco en la Ventana (tengo y no tengo el foco)

---
+++ src/java_hasta_el_video_100/video75_1/FocoVentana.java
@@ -0,0 +1,72 @@
+
+package java_hasta_el_video_100.video75_1;
+  
+import java.awt.event.*;
+import javax.swing.JFrame;
+
+
+  
+
+public class FocoVentana extends JFrame implements WindowFocusListener{
+
+     
+    public static void main(String[] args) {
+        
+        FocoVentana miventana = new FocoVentana();
+        
+        miventana.iniciar();
+        
+         
+    }
+    
+    //Metodo donde construyo los marcos
+    public void iniciar(){
+        
+        marco1 = new FocoVentana();
+        
+        marco2 = new FocoVentana();
+        
+        marco1.setVisible(true);
+        
+        marco2.setVisible(true);
+        
+        marco1.setBounds(200, 100, 400, 350);
+        
+        marco2.setBounds(700, 100, 400, 350);
+        
+        marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        marco2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        marco1.addWindowFocusListener(this);
+        
+        marco2.addWindowFocusListener(this);
+        
+        
+    }
+
+    @Override
+    public void windowGainedFocus(WindowEvent e) {
+        
+        if(e.getSource()==marco1){
+            marco1.setTitle("Tengo el foco!!");
+        }else {
+            marco2.setTitle("Tengo el foco!!");
+        } 
+    }
+
+    @Override
+    public void windowLostFocus(WindowEvent e){
+         if(e.getSource()==marco1){
+            marco1.setTitle("NO!!! Tengo el foco!!");
+        }else {
+            marco2.setTitle("NO!!! Tengo el foco!!");
+        }
+    }
+    
+    FocoVentana marco1;
+    
+    FocoVentana marco2;
+    
+}
+ 
