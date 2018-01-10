5806bcae373f49094dc3bd5f0f267c28f765d568
german <germanjongewaard@gmail.com>
2018-01-10 22:24:45 +0100

    Panel_Layout extends JPanel

---
+++ src/java_hasta_el_video_100/video81/Layout_I.java
@@ -0,0 +1,67 @@
+
+package java_hasta_el_video_100.video81;
+ 
+import java.awt.FlowLayout;
+import java.awt.event.*;
+
+import javax.swing.*;
+ 
+public class Layout_I {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Marco_Layout mimarco = new Marco_Layout(); 
+        
+        mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        mimarco.setVisible(true);
+    }
+    
+}
+
+ class Marco_Layout extends JFrame{
+    
+    public Marco_Layout(){
+        
+       setTitle("Prueba Acciones");
+        
+       setBounds(400, 150, 600, 300);
+       
+       Panel_Layout lamina = new Panel_Layout();
+       
+       //FlowLayout disposicion = new FlowLayout(FlowLayout.CENTER);
+       
+       //lamina.setLayout(disposicion);   
+       
+       //Una diferente disposicion a la que esta comentada arriba. pero hace lo mismo
+       lamina.setLayout(new FlowLayout(FlowLayout.LEFT));
+       
+       
+       
+       
+       
+       add(lamina);
+         
+    }    
+}
+
+class Panel_Layout extends JPanel  {
+
+  public Panel_Layout(){
+      
+      add(new JButton("Amarillo"));
+      
+      add(new JButton("Rojo"));
+      
+      add(new JButton("Azul"));
+      
+      
+       
+      
+  }
+}
+
+   
