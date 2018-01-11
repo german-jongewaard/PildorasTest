2aa93c79e8a7aa071748092d116b254c1908c3db
german <germanjongewaard@gmail.com>
2018-01-11 20:47:44 -0300

    Comprueba si la contraseña es correcta CampoPassword

--- src/java_hasta_el_video_100/video89/CampoPassword.java
+++ src/java_hasta_el_video_100/video89/CampoPassword.java
@@ -1,23 +1,21 @@
- 
-package java_hasta_el_video_100.video89; 
+ package java_hasta_el_video_100.video89; 
   
  
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.*;
-
 import javax.swing.event.*;
-
 import javax.swing.text.*;
 
+ /**
+ *
+ * @author german
+ * 
+ */
  
 public class CampoPassword {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
         MarcoPassword mimarco = new MarcoPassword(); 
