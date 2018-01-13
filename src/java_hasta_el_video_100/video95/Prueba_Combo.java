5ef6efc0509cb6d1a38262acdff6a542b1557874
german <germanjongewaard@gmail.com>
2018-01-13 21:26:46 +0100

    Probando ComboBox con tipos de texto

--- src/java_hasta_el_video_100/video95/Prueba_Combo.java
+++ src/java_hasta_el_video_100/video95/Prueba_Combo.java
@@ -1,12 +1,8 @@
- 
-package java_hasta_el_video_100.video95;   
+ package java_hasta_el_video_100.video95;   
  
 import java.awt.BorderLayout;
-
 import java.awt.Font;
-
 import java.awt.event.*; 
-
 import javax.swing.*;
  
 
