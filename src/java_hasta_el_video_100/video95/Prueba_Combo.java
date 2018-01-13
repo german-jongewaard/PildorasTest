619a19faf3cce165c516f098e4b39a16d52541c2
german <germanjongewaard@gmail.com>
2018-01-13 21:26:14 +0100

    Probando ComboBox con tipos de texto

---
+++ src/java_hasta_el_video_100/video95/Prueba_Combo.java
@@ -0,0 +1,96 @@
+ 
+package java_hasta_el_video_100.video95;   
+ 
+import java.awt.BorderLayout;
+
+import java.awt.Font;
+
+import java.awt.event.*; 
+
+import javax.swing.*;
+ 
+
+ 
+public class Prueba_Combo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Marco_combo mimarco = new Marco_combo(); 
+        
+        mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);          
+        
+        mimarco.setVisible(true); 
+    }    
+}
+
+ class Marco_combo extends JFrame{
+    
+    public Marco_combo(){
+        
+       setVisible(true);
+       
+       setTitle("Probando ComboBox con tipos de texto");
+        
+       setBounds(550, 300, 500, 300);  
+       
+       Lamina_combo milamina = new Lamina_combo();
+       
+       add(milamina);
+       
+    }    
+} 
+
+class Lamina_combo extends JPanel{
+    public Lamina_combo(){
+          
+        setLayout(new BorderLayout());
+        
+        texto = new JLabel("En un lugar de la mancha de cuyo nombre...");
+        
+        texto.setFont(new Font("Serif", Font.PLAIN, 18));
+        
+        add(texto, BorderLayout.CENTER);
+        
+        JPanel lamina_norte = new JPanel();
+        
+        micombo = new JComboBox();
+        
+        micombo.setEditable(true);
+        
+        micombo.addItem("Serif");        
+        
+        micombo.addItem("SansSerif");
+        
+        micombo.addItem("Monospaced");        
+        
+        micombo.addItem("Dialog");
+        
+        Evento_combo mievento = new Evento_combo();
+        
+        micombo.addActionListener(mievento);
+        
+        lamina_norte.add(micombo);
+        
+        add(lamina_norte, BorderLayout.NORTH); 
+         
+    } 
+    
+    private class Evento_combo implements ActionListener{
+
+         
+        public void actionPerformed(ActionEvent e) {
+                        //Hago un Casting para que mi combo se convierta en String
+            texto.setFont(new Font((String)micombo.getSelectedItem(), Font.PLAIN, 18));
+        }
+        
+        
+    }
+    
+    private JLabel texto;
+    
+    private JComboBox micombo;
+}
+     
