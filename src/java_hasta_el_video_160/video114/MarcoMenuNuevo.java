c5195563d125229f485e8bc282de7c37cbbaa6ae
german <germanjongewaard@gmail.com>
2018-01-28 02:53:49 +0100

    Implementación Barra de herramientas extraible - Con mas opciones III

--- src/java_hasta_el_video_160/video114/MarcoMenuNuevo.java
+++ src/java_hasta_el_video_160/video114/MarcoMenuNuevo.java
@@ -148,8 +148,8 @@ class LaminaProcesador extends JPanel{
         configura_barra("src/java_hasta_el_video_160/video114/graficos/subrayado.gif").addActionListener(new StyledEditorKit.UnderlineAction());
         
         configura_barra("src/java_hasta_el_video_160/video114/graficos/azul.gif").addActionListener(new StyledEditorKit.ForegroundAction("Pone_azul", Color.BLUE));
-        configura_barra("src/video114/graficos/amarilla.gif").addActionListener(new StyledEditorKit.ForegroundAction("Pone_amarilla", Color.YELLOW));
-        configura_barra("src/video114/graficos/roja.gif").addActionListener(new StyledEditorKit.ForegroundAction("Pone_roja", Color.RED));
+        configura_barra("src/java_hasta_el_video_160/video114/graficos/amarilla.gif").addActionListener(new StyledEditorKit.ForegroundAction("Pone_amarilla", Color.YELLOW));
+        configura_barra("src/java_hasta_el_video_160/video114/graficos/roja.gif").addActionListener(new StyledEditorKit.ForegroundAction("Pone_roja", Color.RED));
         
         configura_barra("src/video114/graficos/a_izquierda.gif").addActionListener(new StyledEditorKit.AlignmentAction("Texto_Izquierda", 0));
         configura_barra("src/video114/graficos/a_centro.gif").addActionListener(new StyledEditorKit.AlignmentAction("Texto_Centrado", 1));
