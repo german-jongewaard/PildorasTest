4b3e3b9a994dae4ddeb68f91d0e3d08472440d36
german <germanjongewaard@gmail.com>
2018-01-29 23:48:32 +0100

    Implementación Marco en Caja, Nombre y Contraseña.

--- src/java_hasta_el_video_160/video115/PruebaDisposiciones.java
+++ src/java_hasta_el_video_160/video115/PruebaDisposiciones.java
@@ -12,9 +12,7 @@ import javax.swing.*;
  
 public class PruebaDisposiciones {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
         
