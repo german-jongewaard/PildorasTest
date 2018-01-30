995481e60a7f6adf09fd29f23bb05fca01eac187
german <germanjongewaard@gmail.com>
2018-01-30 22:25:33 +0100

    Implementación Marco Nombre Apellido.

---
+++ src/java_hasta_el_video_160/video118/LibreDisposicion.java
@@ -0,0 +1,73 @@
+ 
+package video118;     
+ 
+import java.awt.BorderLayout;
+import java.awt.Dimension; 
+
+import javax.swing.*; 
+
+ 
+public class LibreDisposicion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       MarcoLibre mimarco = new MarcoLibre();
+       
+       mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       
+        
+    }    
+}
+ 
+
+class MarcoLibre extends JFrame{
+    
+    public MarcoLibre(){  
+        
+        setTitle("Marco Libre");
+        
+        setBounds(450, 200, 600, 400);  
+        
+        LaminaLibre milamina = new LaminaLibre();
+        
+        add(milamina);
+        
+        setVisible(true); 
+    } 
+}
+
+class LaminaLibre extends JPanel{
+    public LaminaLibre(){ 
+        
+        //Un layout nulo, una disposición LIBRE!!!
+        setLayout(null);
+        
+        JLabel nombre = new JLabel("Nombre: ");
+        
+        JLabel apellido = new JLabel("Apellido: ");
+        
+        JTextField c_nombre = new JTextField(10);
+        
+        JTextField c_apellido = new JTextField(20);
+        
+        nombre.setBounds(20, 20, 80, 10);
+        
+        c_nombre.setBounds(100, 17, 100, 20);
+        
+        apellido.setBounds(20, 60, 80, 15);
+        
+        c_apellido.setBounds(100, 55, 100, 20);
+        
+        
+        add(nombre);
+        add(apellido);
+        add(c_nombre);
+        add(c_apellido);
+    }
+}
+ 
+     
