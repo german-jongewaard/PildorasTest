9e99b778c1efbab1a57c8aed5d14d6cc4026f1a3
german <germanjongewaard@gmail.com>
2018-01-30 22:33:26 +0100

    Implementación Marco Nombre Apellido.

--- src/java_hasta_el_video_160/video118/LibreDisposicion.java
+++ src/java_hasta_el_video_160/video118/LibreDisposicion.java
@@ -12,9 +12,6 @@ import javax.swing.*;
  
 public class LibreDisposicion {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         
