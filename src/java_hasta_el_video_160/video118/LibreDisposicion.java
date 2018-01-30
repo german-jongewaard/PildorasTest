18897c3fab6fe3ffe1c7574059de6d04d8ec12f6
german <germanjongewaard@gmail.com>
2018-01-30 22:32:42 +0100

    Implementación Marco Nombre Apellido.

--- src/java_hasta_el_video_160/video118/LibreDisposicion.java
+++ src/java_hasta_el_video_160/video118/LibreDisposicion.java
@@ -1,9 +1,7 @@
- 
-package java_hasta_el_video_160.video118;     
- 
+package java_hasta_el_video_160.video118;   
+
 import java.awt.BorderLayout;
 import java.awt.Dimension; 
-
 import javax.swing.*; 
 
  
