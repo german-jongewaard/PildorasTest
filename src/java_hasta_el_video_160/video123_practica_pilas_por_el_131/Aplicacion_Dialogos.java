74ec695c4dc38150e0c4969359d30fc5b6096ca2
german <germanjongewaard@gmail.com>
2018-02-06 22:51:00 +0100

    Implementación App Diálogos

---
+++ src/java_hasta_el_video_160/video123_practica_pilas_por_el_131/Aplicacion_Dialogos.java
@@ -0,0 +1,19 @@
+ 
+package java_hasta_el_video_160.video123_practica_pilas_por_el_131;
+ 
+ 
+import javax.swing.*;
+ 
+public class Aplicacion_Dialogos {
+ 
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Marco_Dialogos mimarco = new Marco_Dialogos(); 
+        
+        mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        mimarco.setVisible(true);
+    }
+    
+}
+ 
