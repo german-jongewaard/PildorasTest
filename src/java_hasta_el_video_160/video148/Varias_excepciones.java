dbfd06214b541ae02d4b0dea3ffdc7e4761e65cf
german <germanjongewaard@gmail.com>
2018-02-25 21:47:50 +0100

    Implementando dividendo divisor y Varias Excepciones.

---
+++ src/java_hasta_el_video_160/video148/Varias_excepciones.java
@@ -0,0 +1,54 @@
+package video148;
+
+
+ 
+import java.io.*;
+
+import javax.swing.*;
+
+
+public class Varias_excepciones {
+
+    
+    public static void main(String[] args) {
+        
+        try{
+         
+        division();
+        
+        }catch(ArithmeticException e){
+            
+            System.out.println("Estás diviendiendo por cero");
+            
+        }catch(NumberFormatException e){
+            
+            System.out.println("No has introducido un número entero");
+            
+            //System.out.println(e.getMessage());
+            
+            System.out.println("se ha generado un error de este tipo " + 
+                    e.getClass().getName());
+            /* aqui soy mas especifico y me muestra el nombre de la clase del error*/
+            
+        
+        }
+     
+          
+        
+    }
+                                        
+    static void division()  {
+        
+        int num1 = Integer.parseInt(JOptionPane.showInputDialog("Introduce el dividendo"));
+        
+        int num2 = Integer.parseInt(JOptionPane.showInputDialog("Introduce el divisor"));
+        
+        System.out.println("El resultado es: " + num1/num2);
+              
+        }
+        
+       
+        
+    
+    
+}
