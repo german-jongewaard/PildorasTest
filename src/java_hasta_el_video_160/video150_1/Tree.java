988dc9b4599f6ca913e1d0a476afb16c031b674a
german <germanjongewaard@gmail.com>
2018-03-07 18:29:27 +0100

    Implementando BinaryTreeAlgorithm - Prueba. - No Video.

---
+++ src/java_hasta_el_video_160/video150_1/Tree.java
@@ -0,0 +1,12 @@
+package java_hasta_el_video_160.video150_1;
+
+public class Tree<T> {
+	public T value;
+	public Tree<T> left;
+	public Tree<T> right;
+	
+	public Tree(T value){
+		this.value = value;
+	}
+
+}
