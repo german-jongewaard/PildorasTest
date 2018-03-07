be1847063a238026eee300af6836cb7ac7e6892d
german <germanjongewaard@gmail.com>
2018-03-07 18:31:48 +0100

    Implementando BinaryTreeAlgorithm - Prueba. - No Video.

---
+++ src/test_gral/inorder_preorder/Tree.java
@@ -0,0 +1,12 @@
+package test_gral.inorder_preorder;
+
+public class Tree<T> {
+	public T value;
+	public Tree<T> left;
+	public Tree<T> right;
+	
+	public Tree(T value){
+		this.value = value;
+	}
+
+}
