d29d90e78cf8b37d5580081cdf48afe750c7a065
german <germanjongewaard@gmail.com>
2018-03-09 19:26:33 +0100

    Implementando OOD_TermProject - Prueba. - No Video.

---
+++ src/test_gral/OOD_TermProject_emptyPlaylist_v2/structuralPattern/WavPlayer.java
@@ -0,0 +1,41 @@
+package test_gral.OOD_TermProject_emptyPlaylist_v2.structuralPattern;
+
+import java.io.File;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+/**
+ *
+ * @author German
+ * 
+ */
+
+public class WavPlayer implements AdvancedMediaPlayer {
+
+	private Clip clip;
+	
+	public WavPlayer(){	
+	}
+	
+	@Override
+	public void playMedia(File fileName) {
+		try{
+			clip = AudioSystem.getClip();
+			clip.open(AudioSystem.getAudioInputStream(fileName));
+			clip.start(); 
+			
+//			Thread.sleep(clip.getMicrosecondLength()/1000);
+			
+		}catch(Exception e){
+			
+		}
+	}
+
+	@Override
+	public void stopMedia(File fileName) {
+		if(clip != null){
+			clip.stop();
+		}
+		// clip = null;
+	} 
+}
