2efa905599a99e156d92182ec3ae7d8f187c9676
german <germanjongewaard@gmail.com>
2018-03-09 19:25:09 +0100

    Implementando OOD_TermProject - Prueba. - No Video.

---
+++ src/test_gral/OOD_TermProject_emptyPlaylist_v2/structuralPattern/MediaPlayer.java
@@ -0,0 +1,14 @@
+package test_gral.OOD_TermProject_emptyPlaylist_v2.structuralPattern;
+
+import java.io.File;
+
+/**
+ *
+ * @author German
+ * 
+ */
+
+public interface MediaPlayer {
+	public void play(File fileName);
+	public void stop(File fileName);
+}
