040a824c5963630dbb65e7d22608c5e65b3badf3
german <germanjongewaard@gmail.com>
2018-03-09 19:21:40 +0100

    Implementando OOD_TermProject - Prueba. - No Video.

---
+++ src/test_gral/OOD_TermProject_emptyPlaylist_v2/structuralPattern/AdvancedMediaPlayer.java
@@ -0,0 +1,8 @@
+package test_gral.OOD_TermProject_emptyPlaylist_v2.structuralPattern;
+
+import java.io.File;
+
+public interface AdvancedMediaPlayer {
+	 public void playMedia(File fileName);
+	 public void stopMedia(File fileName);
+}
