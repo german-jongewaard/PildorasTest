748a0fbcda0ebc3828145b568d989210196ffc2b
german-jongewaard <germanjongewaard@gmail.com>
2019-03-02 09:49:49 +0100

    Exercises...

---
+++ src/ExercisesonJavaBasics/PrintNumberInWord_1_4.java
@@ -0,0 +1,23 @@
+/*
+ * 1.4  PrintNumberInWord (nested-if, switch-case)
+ *
+ * Write a program called PrintNumberInWord which prints 
+ * "ONE", "TWO",... , "NINE", "OTHER" if the int variable "number" 
+ * is 1, 2,... , 9, or other, respectively. Use (a) a "nested-if" 
+ * statement; (b) a "switch-case-default" statement.
+ *
+ *
+ */
+package ExercisesonJavaBasics;
+
+/**
+ *
+ * @author german
+ */
+public class PrintNumberInWord_1_4 {
+ 
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
