100ac7984c6ecdca77a6e043bc5a660ad6fb1bf2
german-jongewaard <germanjongewaard@gmail.com>
2019-03-07 18:30:27 +0100

    El radio de un círculo y calcular su área.

---
+++ src/Ej_Prog_Java/Radio_long_3.java
@@ -0,0 +1,33 @@
+package Ej_Prog_Java;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author german
+ * 
+ * Pedir el radio de una circunferencia y calcular su longitud.
+ * 
+ */
+
+public class Radio_long_3 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        double l,r; // longitud y radio
+        
+        System.out.print("Introduce el radio de una circunferencia: ");
+        
+        r = entrada.nextInt();
+        
+        l = 2 * Math.PI * r;
+        
+        System.out.println("La longitud de una circunferencia de radio " + r + " es: " + l);
+        
+        
+    }
+    
+}
