84b1cd15500fa36f4d803e7d9311a7e39018e08d
german-jongewaard <germanjongewaard@gmail.com>
2019-03-10 20:56:49 +0100

    video184 Colecciones VI LinkedList

---
+++ src/java_hasta_el_video_200/video184/ColeccionesVILinkedList.java
@@ -0,0 +1,14 @@
+ package java_hasta_el_video_200.video184;
+
+/**
+ *
+ * @author german
+ */
+ 
+public class ColeccionesVILinkedList {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
