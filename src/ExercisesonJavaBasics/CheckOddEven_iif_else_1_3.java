1f2fd55ed96766b3f17bfcbf931bef26fbbc7143
german-jongewaard <germanjongewaard@gmail.com>
2019-03-11 00:15:36 +0100

    Exercises...

---
+++ src/ExercisesonJavaBasics/CheckOddEven_iif_else_1_3.java
@@ -0,0 +1,33 @@
+/*
+ * Write a program called CheckOddEven which prints "Odd Number" if the 
+int variable “number” is odd, or “Even Number” otherwise. The program shall 
+always print “bye!” before exiting.
+
+Hints
+
+n is an even number if (n % 2) is 0; otherwise, it is an odd number. 
+Use == for comparison, e.g., (n % 2) == 0.
+ */
+package ExercisesonJavaBasics;
+
+/**
+ *
+ * @author german
+ */
+public class CheckOddEven_iif_else_1_3 {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+
+
+/*
+
+    Try number = 0, 1, 88, 99, -1, -2 and verify your results.
+
+    Again, take note of the source-code identation! Make it a good 
+    habit to ident your code properly, for ease of reading your program.
+
+*/
