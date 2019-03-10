b0691e062b9fd71ed10a3d1d6aa6fbb21d27a266
german-jongewaard <germanjongewaard@gmail.com>
2019-03-11 00:15:44 +0100

    Exercises...

---
+++ src/ExercisesonJavaBasics/HelloWorld_1_1.java
@@ -0,0 +1,24 @@
+/*
+ * 
+    Install JDK on your machine. Follow the instructions in "How to Install JDK".
+    Write a Hello-world program using JDK and a source-code editor, such as:
+        For All Platforms: Sublime Text, Atom
+        For Windows: TextPad, NotePad++
+        For Mac OS X: jEdit, gedit
+        For Ubuntu: gedit
+    Read "Introduction to Java Programming for Novices & First-Time Programmers". Do ALL the exercises.
+
+ */
+package ExercisesonJavaBasics;
+
+/**
+ *
+ * @author german
+ */
+public class HelloWorld_1_1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+    }    
+}
