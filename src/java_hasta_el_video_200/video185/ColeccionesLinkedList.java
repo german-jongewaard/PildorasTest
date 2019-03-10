1e3fdbcdc8413359c68a6051f0ccbbc61da1acf5
german-jongewaard <germanjongewaard@gmail.com>
2019-03-10 21:34:12 +0100

    video185 Colecciones VII LinkedList

---
+++ src/java_hasta_el_video_200/video185/ColeccionesLinkedList.java
@@ -0,0 +1,35 @@
+package java_hasta_el_video_200.video185;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author german
+ * 
+ */
+
+public class ColeccionesLinkedList {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        LinkedList<String> paises = new LinkedList<String>();
+        
+        paises.add("España");
+        paises.add("Colombia");
+        paises.add("Mexico");
+        paises.add("Perú");
+        
+        LinkedList<String> capitales = new LinkedList<String>();
+        
+        capitales.add("Madrid");
+        capitales.add("Bogota");
+        capitales.add("DF");
+        capitales.add("Lima");
+        
+        //System.out.println(paises);        
+        //System.out.println(capitales);
+        
+    }
+    
+}
