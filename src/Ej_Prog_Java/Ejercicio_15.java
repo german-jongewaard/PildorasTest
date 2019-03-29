3b84dcad1d736968dcfd4ad9ca1fb77ec2821440
german-jongewaard <germanjongewaard@gmail.com>
2019-03-29 23:51:17 +0100

    Ejercicios básicos

--- src/Ej_Prog_Java/Ejercicio_15.java
+++ src/Ej_Prog_Java/Ejercicio_15.java
@@ -33,49 +33,18 @@ public class Ejercicio_15 {
         anio = numero.nextInt();
                 
         
-        if(anio == 0){
-            System.out.println("fecha Incorrecta");
-        }else{
-             if(mes == 2 &&( dia >=1 && dia <=28)){
-                 System.out.println(dia + "/" + mes + "/" + anio +  ": La fecha es CORRECTA");                 
-             }else if((mes == 4 || mes == 6 || mes == 9 || mes == 11) && ( dia >=1 && dia <=30))  {
-                        
-                      System.out.println(dia + "/" + mes + "/" + anio +  ": La fecha es CORRECTA");    
-             }else if((mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12 ) && ( dia >=1 && dia <=31))  {
-                        
-                      System.out.println(dia + "/" + mes + "/" + anio +  ": La fecha es CORRECTA");    
-             }else{
-                      System.out.println(dia + "/" + mes + "/" + anio +  ": La fecha es INCORRECTA !!");    
-             }
-        }
-        
-        //******************************************************************
-        
-        int dias_del_mes;
-        boolean fecha_correcta=true;
-                
-        dias_del_mes = 0; // si se utiliza un mes fuera del rango 1..12
-        // supondremos que los días del mes son 0.
-        if(anio==0) // el único año que no existe es el 0
-            fecha_correcta = false;
-        if (dia<1 || dia >31) // un día fuera del rango 1..31 no tiene sentido
-            fecha_correcta = false;
-        if (mes<1 || mes >12) // un mes fuera del rango 1..12 no tiene sentido
-            fecha_correcta = false;
-        if(mes==2 )
-            dias_del_mes = 28;
-        if(mes==4 || mes==6 || mes==9 || mes==11)
-            dias_del_mes = 30;
-        if(mes==1 || mes==3 || mes==5 || mes==7 || mes==8 || mes==10 || mes==12)
-            dias_del_mes = 31;
-        if (dia > dias_del_mes)
-            fecha_correcta = false;
-        
-        if (fecha_correcta)
-            System.out.println(dia + "/" + mes + "/" + anio+": Fecha correcta");
+        if(dia >=1 && dia <=30)
+            if(mes >= 1 &&  mes <=12)
+                if(anio != 0)
+                    System.out.println(dia + "/" + mes + "/" + anio +  ": La fecha es CORRECTA");                 
+                else    
+                    System.out.println(dia + "/" + mes + "/" + anio +  ": El año es INCORRECTO");                 
+            else
+                System.out.println(dia + "/" + mes + "/" + anio +  ": El mes es INCORRECTO");                 
         else
-            System.out.println("Fecha incorrecta");
+            System.out.println(dia + "/" + mes + "/" + anio +  ": El día es INCORRECTO");                 
         
+      
         
         
         
